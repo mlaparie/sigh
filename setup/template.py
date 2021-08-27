f155d22b6bfbfc73b87f8a6b27c893e057cc4c22
M <matf@disr.it>
2021-08-27 18:32:27 +0200

    Start thinking about interactive mode and more fzf interactions between functions

--- setup/template.py
+++ setup/template.py
@@ -19,6 +19,12 @@ if __name__ == "__main__":
     locs = [ x.text.replace('\n', '').strip(' ')  for x in  sreplacemes.findAll("td", {"class": "location"}) ]
     titles = [ x.text.replace('\n', '').strip(' ')  for x in  sreplacemes.findAll("h5", {"class": "title"}) ]
     details = [ x.find("a", href=True)["href"]  for x in  sreplacemes.findAll("td", {"class": "details"}) ]
-    json_data = json.dumps({"Date": replaceme, "Location": locs, "Title": titles, "Details": details})
-    print(json_data, file=open(wedir+'/data/replaceme.json', 'a'))
+    json_data = {"Date": replaceme, "Location": locs, "Title": titles, "Details": details}
+
+    result = []
+    for d, l, t, x in zip(json_data["Date"], json_data["Location"], json_data["Title"], json_data["Details"]):
+        result.append({"Date": d, "Location": l, "Title": t, "Details": x})
+
+    result = json.dumps(result)
+    print(result, file=open(wedir+'/data/replaceme.json', 'a'))
     print(f'Appended {len(replaceme)} event(s) to {wedir}/data/replaceme.json. \033[32;1m✔\033[0m')
