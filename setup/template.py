dff25206f426cc8f13a29533eda1d712e398dd33
K <matf@disr.it>
2021-08-27 02:16:05 +0200

    Rewrite with a master script

--- setup/template.py
+++ setup/template.py
@@ -4,10 +4,12 @@
 # Modified by Kabouik, 2021
 # Dependencies: bs4, requests, json
 
+import os
 import requests
 import json
 from bs4 import BeautifulSoup
 
+wedir=os.environ["WE_DIR"]
 if __name__ == "__main__":
     res = requests.get("https://rsoe-edis.org/eventList")
     content = res.text
@@ -17,5 +19,5 @@ if __name__ == "__main__":
     locs = [ x.text.replace('\n', '').strip(' ')  for x in  sreplacemes.findAll("td", {"class": "location"}) ]
     titles = [ x.text.replace('\n', '').strip(' ')  for x in  sreplacemes.findAll("h5", {"class": "title"}) ]
     details = [ x.find("a", href=True)["href"]  for x in  sreplacemes.findAll("td", {"class": "details"}) ]
-    print(json.dumps({"Date": replaceme, "Location": locs, "Title": titles, "Details": details}), file=open('data/replaceme.json', 'a'))
-    print('✔ Appended data to data/replaceme.json.')
+    print(json.dumps({"Date": replaceme, "Location": locs, "Title": titles, "Details": details}), file=open(wedir+'/data/replaceme.json', 'a'))
+    print('Appended data to '+wedir+'/data/replaceme.json. ✔')
