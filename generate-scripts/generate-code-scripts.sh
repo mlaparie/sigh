4f32bb7836cd1e99fda1625c6322a6db2f75cef9
Mathieu <mathieu.laparie@inrae.fr>
2024-04-24 17:49:49 +0200

    Initial commit

---
+++ generate-scripts/generate-code-scripts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Generate individual .py files for each "subList" code from rsoe.edis.org.
+# To get all subList codes, go to the rsoe events list, edit page source, and
+# find each "subList-" occurrence, they are appended with codes.
+# Save codes in cats.txt, with one code per line.
+# (Can be done easily with kakoune and `%ssubList-w<ret>y%<a-d><a-p>`.
+
+while read -r code;
+    do sed "s/replaceme/$code/g" template.py > "../$code.py";
+    chmod +x "../$code.py";
+    printf "$code.py"; printf '\033[32;1m %s \033[0m\n' "✔" ;
+done < codes.txt
---
+++ generate-scripts/template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Scrap event information from rsoe.edis.org
+# Copyright aetin, see gemini://aetin.art/earth.gmi
+# Modified by Kabouik, 2021
+# Dependencies: bs4, requests, json, beautifulsoup, jtbl
+
+import requests
+import json
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__":
+    res = requests.get("https://rsoe-edis.org/eventList")
+    content = res.text
+    soup = BeautifulSoup(content, "lxml")
+    sreplacemes = soup.find(id="subList-replaceme")
+    replaceme = [ x.text.replace('\n', '').strip(' ')  for x in  sreplacemes.findAll("td", {"class": "eventDate"}) ]
+    locs = [ x.text.replace('\n', '').strip(' ')  for x in  sreplacemes.findAll("td", {"class": "location"}) ]
+    titles = [ x.text.replace('\n', '').strip(' ')  for x in  sreplacemes.findAll("h5", {"class": "title"}) ]
+    details = [ x.find("a", href=True)["href"]  for x in  sreplacemes.findAll("td", {"class": "details"}) ]
+    print(json.dumps({"Date": replaceme, "Location": locs, "Title": titles, "Details": details}))
